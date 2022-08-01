4951a355fe48273cd70d27e3c06ddc49a5555e6d
Bairon00 <bairon97@icloud.com>
2022-08-01 03:49:06 +0000

    casi terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#numero").innerHTML = numberRandom();
+  document.querySelector("#logo").innerHTML = jaja;
+  document.querySelector("#logo2").innerHTML = jaja;
+
+  //escoje un numero
+  function numberRandom() {
+    let numeros = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let random = Math.floor(Math.random() * numeros.length);
+    return numeros[random];
+  }
+  function iconoRandom() {
+    let iconos = ["♦", "♥", "♠", "♣"];
+    let icrandom = Math.floor(Math.random() * iconos.length);
+    return iconos[icrandom];
+  }
+  let jaja = iconoRandom();
+  //function iconoRandom2() {
+  //let iconos2 = ["♦", "♥", "♠", "♣"];
+  //let icrandom2 = Math.floor(Math.random() * iconos2.length);
+  //return iconos2[icrandom2];
+  //}
+  //console.log("Hello Rigo from the console!");
 };
